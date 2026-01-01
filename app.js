e8638a48a5019e29744d3b9be4818a7f1fc174a0
Saul Good <saulgood04@gmail.com>
2025-12-31 19:20:43 -0500

    added middleware

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ mongoose
   .catch(console.error);
 
 app.use(express.json());
+app.use((req, res, next) => {
+  req.user = {
+    _id: "5d8b8592978f8bd833ca8133"
+  };
+  next();
+});
 app.use("/", mainRouter);
 
 app.listen(PORT, () => {});
