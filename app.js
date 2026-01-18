317365ca14a98e6e486be8335b7a4a641ddcc9a5
Saul Good <saulgood04@gmail.com>
2026-01-18 13:29:06 -0500

    fixed github issue

--- app.js
+++ app.js
@@ -15,6 +15,12 @@ mongoose
 
 app.use(express.json());
 app.use(cors());
+app.use((req, res, next) => {
+  req.user = {
+    _id: "5d8b8592978f8bd833ca8133"
+  };
+  next();
+});
 
 app.post('/signin', login);
 app.post('/signup', createUser);
